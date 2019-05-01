19f12b326fd395ccdac02c9908116645d59d92e7
Alex Mota <alexmota5@hotmail.com>
2019-05-01 12:30:13 -0300

    add service gateway

---
+++ pedido/src/main/java/br/com/srm/Application.java
@@ -0,0 +1,21 @@
+package br.com.srm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@RefreshScope
+@SpringBootApplication
+@EnableMongoRepositories
+@EnableFeignClients
+@EnableHystrix
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
